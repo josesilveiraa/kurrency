4682bc094bf84217e9d3e0ca1877fb25c83b87b5
josesilveira <josesilveiraa@protonmail.com>
2022-03-26 18:12:35 -0300

    [fix] add database connection before every transaction

--- src/main/kotlin/com/josesilveiraa/kurrency/listener/PlayerJoinListener.kt
+++ src/main/kotlin/com/josesilveiraa/kurrency/listener/PlayerJoinListener.kt
@@ -1,7 +1,7 @@
 package com.josesilveiraa.kurrency.listener
 
 import com.josesilveiraa.kurrency.Kurrency
-import com.josesilveiraa.kurrency.manager.UserManager
+import com.josesilveiraa.kurrency.manager.SqlManager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -13,12 +13,12 @@ class PlayerJoinListener : Listener {
         val player = event.player
         val uuid: String = player.uniqueId.toString()
 
-        if(!UserManager.exists(uuid)) {
-            UserManager.createUser(uuid, 0.0, 0)
+        if(!SqlManager.exists(uuid)) {
+            SqlManager.createUser(uuid, 0.0, 0)
         }
 
         if(!Kurrency.users.containsKey(uuid)) {
-            val user = UserManager.getUser(uuid)
+            val user = SqlManager.getUser(uuid)
 
             Kurrency.users[uuid] = user!!
         }
--- src/main/kotlin/com/josesilveiraa/kurrency/manager/SqlManager.kt
+++ src/main/kotlin/com/josesilveiraa/kurrency/manager/SqlManager.kt
@@ -1,16 +1,23 @@
 package com.josesilveiraa.kurrency.manager
 
+import com.josesilveiraa.kurrency.Kurrency
 import com.josesilveiraa.kurrency.dataclass.User
 import com.josesilveiraa.kurrency.table.Users
-import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 
-object UserManager {
+object SqlManager {
+
+    private val sqlHost = Kurrency.plugin!!.config.getString("mysql.host")
+    private val sqlPort = Kurrency.plugin!!.config.getString("mysql.port")
+    private val sqlDatabase = Kurrency.plugin!!.config.getString("mysql.database")
+    private val sqlUser = Kurrency.plugin!!.config.getString("mysql.user")
+    private val sqlPassword = Kurrency.plugin!!.config.getString("mysql.password")
 
     fun createUser(playerUuid: String, targetBalance: Double, targetTransactions: Int) {
+
+        Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
+
         transaction {
             Users.insert {
                 it[id] = playerUuid
@@ -21,6 +28,9 @@ object UserManager {
     }
 
     fun getUser(playerUuid: String): User? {
+
+
+        Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
 
         var user: User? = null
 
@@ -38,6 +48,8 @@ object UserManager {
     }
 
     fun updateUserBalance(playerUuid: String, targetBalance: Double) {
+        Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
+
         transaction {
             Users.update({ Users.id eq playerUuid }) {
                 it[Users.balance] = targetBalance
@@ -46,6 +58,8 @@ object UserManager {
     }
 
     fun updateUserTransactions(playerUuid: String, targetTransactions: Int) {
+        Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
+
         transaction {
             Users.update({ Users.id eq playerUuid }) {
                 it[Users.transactions] = targetTransactions
@@ -53,13 +67,17 @@ object UserManager {
         }
     }
 
-    fun deleteUser(playerUuid: String) {
+    fun deleteUser(playerUuid: String) {Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
+        Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
+
         transaction {
             Users.deleteWhere { Users.id eq playerUuid }
         }
     }
 
     fun exists(playerUuid: String): Boolean {
+        Database.connect("jdbc:mysql://$sqlHost:$sqlPort/$sqlDatabase", user = sqlUser!!, password = sqlPassword!!)
+
         var result = false
 
         transaction {
