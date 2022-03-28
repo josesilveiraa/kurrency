3b6ef3462bdc06b95887eb82fcf23453a44aaa3e
josesilveira <josesilveiraa@protonmail.com>
2022-03-27 22:40:02 -0300

    [refactor] remove blank-lines in `User.kt`

--- src/main/kotlin/com/josesilveiraa/kurrency/dataclass/User.kt
+++ src/main/kotlin/com/josesilveiraa/kurrency/dataclass/User.kt
@@ -3,9 +3,7 @@ package com.josesilveiraa.kurrency.dataclass
 import com.josesilveiraa.kurrency.manager.SqlManager
 
 data class User(val uuid: String, val nickname: String, var balance: Double, var transactions: Int) {
-
     fun save() {
         SqlManager.updateUser(nickname, balance, transactions)
     }
-
 }
